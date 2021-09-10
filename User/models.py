5b9e5f03c1f462500ebda2bd67b87646c963b478
jdarshan5 <57329561+jdarshan5@users.noreply.github.com>
2021-09-10 11:52:26 +0530

    removed not used imports

--- User/models.py
+++ User/models.py
@@ -1,4 +1,3 @@
-from django.db.models.expressions import F
 from University.models import TblDepartments, TblInstitutes
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
