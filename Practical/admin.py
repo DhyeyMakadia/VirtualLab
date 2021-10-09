c77f76c15c3f7468d9265cf8f2896fde9d4b4445
jdarshan5 <57329561+jdarshan5@users.noreply.github.com>
2021-10-09 19:43:07 +0530

    Added Model in admin.py site

--- Practical/admin.py
+++ Practical/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
+from Practical.models import TblPractical, TblInputParameter, TblOutputParameter, TblFixedInputParameter, TblFixedOutputParameter, TblMultiplePracticalImages, TblMultipleAppartus, TblMultipleYoutubeLinks, TblMultipleMaterials, TblMultipleInputParameter, TblMultipleOutputParameter, TblMultipleFixedInputParameter, TblMultipleFixedOutputParameter
+
 # Register your models here.
+
+admin.site.register(TblPractical)
+admin.site.register(TblInputParameter)
+admin.site.register(TblOutputParameter)
+admin.site.register(TblFixedInputParameter)
+admin.site.register(TblFixedOutputParameter)
+admin.site.register(TblMultiplePracticalImages)
+admin.site.register(TblMultipleAppartus)
+admin.site.register(TblMultipleYoutubeLinks)
+admin.site.register(TblMultipleMaterials)
+admin.site.register(TblMultipleInputParameter)
+admin.site.register(TblMultipleOutputParameter)
+admin.site.register(TblMultipleFixedInputParameter)
+admin.site.register(TblMultipleFixedOutputParameter)
