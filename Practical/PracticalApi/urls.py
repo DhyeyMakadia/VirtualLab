edcf672a5c72352d4abe3db94ecad51e5713cc1c
jdarshan5 <57329561+jdarshan5@users.noreply.github.com>
2021-10-09 19:44:55 +0530

    fetchPracticalValue and fetchPracticalLinks urls added

--- Practical/PracticalApi/urls.py
+++ Practical/PracticalApi/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import fetch_practicals
+from .views import fetch_practicals, fetch_practical_values, fetch_youtube_links
 
 urlpatterns = [
     path('fetchPracticals/', fetch_practicals),
+    path('fetchPracticalValues/', fetch_practical_values),
+    path('fetchYoutubeLinks/', fetch_youtube_links),
 ]
