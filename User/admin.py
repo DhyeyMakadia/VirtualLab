114817ea8af34c269343127a59fe35b1f9a636fd
jdarshan5 <57329561+jdarshan5@users.noreply.github.com>
2022-02-10 15:41:38 +0530

    New configuration of admin.py in User app

--- User/admin.py
+++ User/admin.py
@@ -1,13 +1,42 @@
+from django import forms
 from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
 
 from .models import Account, TblUsers, TblAdmin, TblPermissions
 
 # Register your models here.
 
-@admin.register(Account)
-class AccountAdmin(admin.ModelAdmin):
-    list_display = ['email','is_admin','is_active','is_staff','is_superuser']
-    
+
+class UserChangeForm(forms.ModelForm):
+    class Meta:
+        model = Account
+        fields = ('email', 'is_active', 'is_staff', 'is_admin')
+
+
+class UserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    fieldsets = (
+        (None, {'fields': ('email', )}),
+        (_('Permissions'), {'fields': ('is_admin', 'is_active', 'is_staff', 'is_superuser', 'last_login', 'date_joined',
+                                       'groups', 'user_permissions')}),
+    )
+    readonly_fields = ('last_login', 'date_joined')
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide', ),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+    list_display = ['email', ]
+    search_fields = ('email', )
+    ordering = ('email', )
+
+
+admin.site.register(Account, UserAdmin)
+
+
 admin.site.register(TblUsers)
 
 @admin.register(TblAdmin)
