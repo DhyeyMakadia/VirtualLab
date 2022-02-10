8d624aa62f7841864c6cf43cb14f0011385a4be8
jdarshan5 <57329561+jdarshan5@users.noreply.github.com>
2022-02-10 15:41:16 +0530

    Configure admin.py

--- Appartus/admin.py
+++ Appartus/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from Appartus.models import TblAppartus, TblAppartusCategory, TblAppartusSubcategory
+
 # Register your models here.
+
+# admin.site.register(TblAppartus)
+# admin.site.register(TblAppartusCategory)
+# admin.site.register(TblAppartusSubcategory)
